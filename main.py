50a1d9285401fe99fd2028b5496844043bff72d7
ankhafizov <ankhafizov1998@yandex.ru>
2021-05-12 19:17:05 +0300

    main changed: parrallel processing added

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
-import json
-from pathlib import Path
-from PIL import Image
-
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage import exposure, morphology
-from skimage import morphology
 from tqdm import tqdm
+from skimage.segmentation import random_walker
+from porespy.filters import chunked_func
+from skimage.filters import median
+from skimage.morphology import ball
 
 import data_manager as dm
-import dots_scanner
-import viewer
+
 
 NUM_OF_TIF_SLICES = 2120
 
 
-def load_data(sample_name, num_of_files=NUM_OF_TIF_SLICES):
-    for n in range(num_of_files):
-        yield dm.get_img2d_from_server(sample_name, n)
-
-
-def generate_sequential_file_names(num_of_files=NUM_OF_TIF_SLICES):
-    name = lambda num: "0" * (4-len(str(num))) + str(num)
-    for n in range(num_of_files):
-        yield name(n)
+def binarize_img(im, thrs1, thrs2):
+    markers = np.zeros_like(im)
+    markers[im > thrs1] = 1
+    markers[im < thrs2] = 2
+    t = random_walker(im, markers, beta=100)
+    
+    return t
 
 
 if __name__ == "__main__":
     num_of_slice = 1001
     sample = "gecko_123438"
-    
-    images = load_data(sample)
-    shot_names = generate_sequential_file_names()
+
+    shot_names = [dm.generate_tif_file_name(n) for n in range(NUM_OF_TIF_SLICES)]
+
+    image_3d = dm.assemble_3d_server(sample, [400, 500])
+    thrs1 = np.percentile(image_3d.flat[::5], 99)
+    thrs2 = np.percentile(image_3d.flat[::5], 50)
+
+    image_3d = median(image_3d, selem=ball(1))
+    print(image_3d.shape)
+
+    image_3d = chunked_func(func=binarize_img,
+                            im=image_3d,
+                            thrs1=thrs1,
+                            thrs2=thrs2,
+                            divs=[2, 20, 20],
+                            overlap=10)
 
     # img2d = dm.get_img2d_from_database('1.tif', 'gecko')
     # for i in file_names:
@@ -41,20 +48,5 @@ if __name__ == "__main__":
     # ax.imshow(next(images))
     # dm.save_plot(fig, "plots", "1")
 
-    for img2d, shot_name in tqdm(zip(images, shot_names), total=NUM_OF_TIF_SLICES):
-        thresh = lambda x: x>np.percentile(x.ravel(), 50)
-        eq_img = exposure.equalize_hist(img2d)
-        
-
-        img2d_mask = thresh(eq_img)
-
-
-        img2d_mask = dots_scanner.get_small_pores_mask(img2d,
-                                                    img2d_mask,
-                                                    percentile_glob=90,
-                                                    min_large_contour_length=1000,
-                                                    window_size=100)
-        img2d_mask = morphology.binary_opening(img2d_mask, selem=morphology.disk(2))
-        
-        
-        dm.save_tif(img2d_mask, sample, shot_name)
+    for img2d, shot_name in tqdm(zip(image_3d, shot_names), total=NUM_OF_TIF_SLICES):
+        dm.save_tif(img2d, sample, shot_name)
