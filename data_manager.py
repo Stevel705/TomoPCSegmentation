4fbf75bffe4702a3673fb86d70b9990185140150
ankhafizov <ankhafizov1998@yandex.ru>
2021-05-16 21:18:06 +0300

    data_manager feature:assembler database parameter num_of_layers

--- data_manager.py
+++ data_manager.py
@@ -129,18 +129,18 @@ def load_data_server(sample_name, numbers_of_files):
         yield get_img2d_from_server(sample_name, fn), generate_tif_file_name(k)
 
 
-def load_data_database(folder_name, count_of_files="all"):
-    if count_of_files=="all":
+def load_data_database(folder_name, numbers_of_files="all"):
+    if numbers_of_files=="all":
         path = os.path.join(SCRIPT_PATH, DATABASE_FOLDER_NAME, folder_name)
-        count_of_files = len(os.listdir(path))
+        numbers_of_files = len(os.listdir(path))
 
-    for i in range(count_of_files):
-        file_name = generate_tif_file_name(i, True)
+    for k in range(*numbers_of_files):
+        file_name = generate_tif_file_name(k, True)
         yield get_img2d_from_database(file_name, folder_name), file_name
 
 
-def assemble_3d_database(folder_name):
-    data = load_data_database(folder_name, count_of_files="all")
+def assemble_3d_database(folder_name, numbers_of_files="all"):
+    data = load_data_database(folder_name, numbers_of_files)
     img3d = [img2d for img2d, _ in data]
     
     return np.array(img3d)
